c987270c632393ba2244ee0883daa517e4dec02d
arun k <kumararun@virtusa.com>
2021-02-03 21:47:19 +0530

    first commit

---
+++ src/main/java/saml/SpringAzureSamlApplication.java
@@ -0,0 +1,13 @@
+package saml;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAzureSamlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAzureSamlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/saml/SpringAzureSamlApplicationTests.java
@@ -0,0 +1,13 @@
+package saml;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAzureSamlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
